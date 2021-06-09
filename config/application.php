498f9297b9919e322dc31d964109912f3c9b7e41
dave coe <hello@davecoe.dev>
2021-06-09 15:16:41 +0100

    changed versions to lando recommended

--- config/application.php
+++ config/application.php
@@ -100,6 +100,7 @@ Config::define('DISALLOW_FILE_MODS', true);
 // Limit the number of post revisions that Wordpress stores (true (default WP): store every revision)
 Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
 
+
 /**
  * Debugging Settings
  */
